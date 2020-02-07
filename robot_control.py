e08eb8b860fded96b019dd999bb709491d29108a
NWiktor <31969508+NWiktor@users.noreply.github.com>
2020-02-07 16:43:47 +0100

    mukodo program
    3 tengely menti interpolacio beepitve
    gpio kiosztas tesztelve
    motorok forognak
    lepteto_motor_felbontas_ellenorizendo

--- robot_control.py
+++ robot_control.py
@@ -1,71 +1,103 @@
 # -*- coding: utf-8 -*-#
 import stepper_mot_control as smc
 import math as m
+import time
 
-motor_step = [4144, 4144]
-resolution = [float(4144/360), float(4144/360)]
-base_frequency = 500 #Hz
-time_unit = float((1/base_frequency)/2)
-actual_position = [0, 0] #step megadva
+#Alap konfig adatok a motorokra vonatkozóan
+motor_step = [4144, 4144, 4144]
+resolution = list(map(lambda x: float(x/360), motor_step))
+# resolution = [float(4144/360), float(4144/360), float(4144/360)]
+base_frequency = 5000 # Hz
+correction = 0 #ms
+time_unit = float( float(1/float(base_frequency)/2) - correction) # ms
+actual_position = [0, 0, 0] # abszolút szög-érték megadva
 
-def move(deg1, deg2): #Megadott szogertekekhez mozgas
+
+def deg_to_step(deg):
+    """ Háromelemű relatív szög-tömbből számol relatív lépésszámot! """
     step = []
-    step.append(deg_to_pos(1,deg1))
-    step.append(deg_to_pos(2,deg2))
-
-
-    #Calc_timetable
-    timetable = []
-    max_step = max(abs(step[0]), abs(step[1]))
-
-    for m in range(1,2):
-        act_time = 0
-        act_step = time_unit * int(max_step / step[m-1])
-
-        for n in range(0, step[m-1]):
-            act_time += act_step
-            timetable.append([act_time, m, 1])
-            act_time += act_step
-            timetable.append([act_time, m, 0])
-
-    time_diff_set 
-
-
-
-
-
-    #Set dir
-    if step[0] < 0: #Motor 1 iranybeallitas
-        smc.dir_set(1,0)
-    else:
-        smc.dir_set(1,1)
-
-    if step[1] < 0: #Motor 2 iranybeallitas
-        smc.dir_set(2,0)
-    else:
-        smc.dir_set(2,1)
-
-
-    #Set enable
-    if step[0] == 0: #Motor 1 iranybeallitas
-        smc.enable_set(1,1)
-    else:
-        smc.enable_set(1,0)
-
-    if step[1] == 0: #Motor 2 iranybeallitas
-        smc.enable_set(2,1)
-    else:
-        smc.enable_set(2,0)
-
-
-    #Iteracio
-    pass
-
-
-def deg_to_pos(mot, deg):
-    step = int(resolution[mot-1] * deg)
+    for k in range(0,len(deg)):
+        # step.append(int(resolution[k] * deg[k]))
+        step.append(m.floor(resolution[k] * deg[k]))
     return step
 
+
+def move(step):
+    """ Megadott lépésszámokhoz (tömb) tartozó mozgásfüggvény """
+    print(step)
+    motor_dir_set(step) #Motorok iránybeállítása
+
+    #3d interpolator
+    # A, B, C tengelyeken lépendő lépések abszolútértéke // a3 =|dx|, b3 =|dy|, c3 =|dz|
+    dp = []
+    for m in range(0,len(step)):
+        dp.append(abs(step[m])) #step értékek abszolútértékének betöltése a dp[] tömbbe
+
+    fab = dp[0] - dp[1] #a3 - b3
+    fac = dp[0] - dp[2] #a3 - c3
+    a2 = b2 = c2 = 0 #aktuális rel pozício
+
+    #Iteracio
+    stepnum = 0 # Hányadik lépésnél tartok
+    # print("Lépés, fab, fac, a2, b2, c2")
+    # print("{0}. lépés: {1}, {2}, {3}, {4}, {5}".format(stepnum, fab, fac, a2, b2, c2))
+
+    #This is where the magic happens!
+    while ((a2 != dp[0]) or (b2 != dp[1]) or (c2 != dp[2])):
+        stepnum += 1
+        if abs(fab) >= abs(fac):
+            if fab > 0:
+                a2 += 1 #motor 1 léptetese
+                onestep_mot(0)
+                fab = fab - dp[1]
+                fac = fac - dp[2]
+            else:
+                b2 += 1 #motor 2 leptetese
+                onestep_mot(1)
+                fab = fab + dp[0]
+        else:
+            if fac > 0:
+                a2 += 1 #motor 1 léptetése
+                onestep_mot(0)
+                fab = fab - dp[1]
+                fac = fac - dp[2]
+            else:
+                c2 += 1 #motor 2 leptetese
+                onestep_mot(2)
+                fac = fac + dp[0]
+
+        #print("{0}. lépés: {1}, {2}, {3}, {4}, {5}".format(stepnum, fab, fac, a2, b2, c2))
+
+
+def onestep_mot(mot):
+    """ Adott motornak 1db négszögjel kiadása """
+    smc.move(mot,1)
+    time.sleep(time_unit)
+    smc.move(mot, 0)
+    time.sleep(time_unit)
+
+
+def motor_dir_set(mot_step):
+    """ Háromelemű tömbnek megfelelően beállítja
+    a motorok forgásirányát """
+
+    for d in range(0,len(mot_step)):
+        if mot_step[d] < 0: #d indexű motor iranybeallitas
+            smc.dir_set(d,0)
+        else:
+            smc.dir_set(d,1)
+
+
+def motor_enable_set(enable):
+    """ Engedélyezi (1) vagy letiltja (0) a motorokat """
+
+    if enable == 1: #Motor 1-3 engedelyezes
+        for s in range(0,3):
+            smc.enable_set(s,1)
+
+    else:           #Motor 1-3 letiltas
+        for s in range(0,3):
+            smc.enable_set(s,0)
 
 
 def reset_pos():
@@ -75,13 +107,19 @@ def reset_pos():
 def zeroing():
     pass
 
+#Főprogram
+if __name__ == "__main__":
+    motor_enable_set(1)
+    move(deg_to_step([180,-180,180]))
+    motor_enable_set(0)
+    smc.cleanup()
 
-try:
-    while True:
-        pass
-
-except:
-    pass
-
-finally:
-    pass
+    # try:
+    #     motor_enable_set(1)
+    #     move(deg_to_pos([25,30,45]))
+    #
+    # except:
+    #     pass
+    #
+    # finally:
+    #     motor_enable_set(0)
--- stepper_mot_control.py
+++ stepper_mot_control.py
@@ -1,54 +1,66 @@
+# -*- coding: utf-8 -*-#
 import RPi.GPIO as gpio
-import time
-import sys
 
-
-gpio.setmode(gpio.BCM)
+gpio.setmode(gpio.BOARD) #BOARD pin számozás használata, mindig változatlan
+# gpio.setmode(gpio.BCM) #Broadcom pin számozás, hardverspecifikus, megváltozhat!
 gpio.setwarnings(False)
 
-#Stepper motorok gpio pinout - tomb formatumban tarolva
-motor_gpio = [17, 0]
-motor_dir = [0, 0]
-motor_enable = [0, 0]
-motor_grab = [0, 1, 2, 3]
+# Stepper motorok gpio pinout - tomb formatumban tarolva
+# Motor 0, 1 és 2 gpio portjai
+motor_gpio = [26, 24, 22]
+motor_dir = [40, 38, 36]
+# motor_enable = [0, 0, 0]
+# motor_grab = [0, 1, 2, 3]
 
-#Gpio setupok
+# Gpio setupok
 gpio.setup(motor_gpio[0], gpio.OUT)
 gpio.setup(motor_gpio[1], gpio.OUT)
+gpio.setup(motor_gpio[2], gpio.OUT)
 gpio.setup(motor_dir[0], gpio.OUT)
 gpio.setup(motor_dir[1], gpio.OUT)
-gpio.setup(motor_enable[0], gpio.OUT)
-gpio.setup(motor_enable[1], gpio.OUT)
-# gpio.setup(motor_grab[0], gpio.OUT)
-# gpio.setup(motor_grab[1], gpio.OUT)
-# gpio.setup(motor_grab[2], gpio.OUT)
-# gpio.setup(motor_grab[3], gpio.OUT)
+gpio.setup(motor_dir[2], gpio.OUT)
+# gpio.setup(motor_enable[0], gpio.OUT)
+# gpio.setup(motor_enable[1], gpio.OUT)
+# gpio.setup(motor_enable[2], gpio.OUT)
+# gpio.setup(motor_grab[0], gpio.OUT, initial=GPIO.LOW)
+# gpio.setup(motor_grab[1], gpio.OUT, initial=GPIO.LOW)
+# gpio.setup(motor_grab[2], gpio.OUT, initial=GPIO.LOW)
+# gpio.setup(motor_grab[3], gpio.OUT, initial=GPIO.LOW)
 
-
-def dir_set(mot, dir): #Mot számú motor iránybeállítása
+def dir_set(mot, dir):
+    """ Adott motor iránybeállítása (CW vagy CCW) """
     if dir == 1:
-        gpio.output(motor_dir[mot-1], gpio.HIGH)
+        gpio.output(motor_dir[mot], gpio.HIGH)
+        print("Motor{0} forgásirány CW!".format(mot+1))
     elif dir == 0:
-        gpio.output(motor_dir[mot-1], gpio.LOW)
+        gpio.output(motor_dir[mot], gpio.LOW)
+        print("Motor{0} forgásirány CCW!".format(mot+1))
     else:
-        print("Hibás iránybeállítás: Motor{0} - Dir:{1}".format(mot, dir))
+        print("Hibás iránybeállítás: Motor{0} - DIR:{1}!".format((mot+1), dir))
 
 
-def enable_set(mot, enable): #Mot számú motor engedélyezése
-    if enable == 1:
-        gpio.output(motor_enable[mot-1], gpio.HIGH)
-    elif enable == 0:
-        gpio.output(motor_enable[mot-1], gpio.LOW)
+def enable_set(mot, enable):
+    """ Adott motor engedélyezése vagy letiltása """
+    if enable == 0:
+        # gpio.output(motor_enable[mot], gpio.HIGH)
+        print("Motor{0} letiltva!".format(mot+1))
+    elif enable == 1:
+        # gpio.output(motor_enable[mot-1], gpio.LOW)
+        print("Motor{0} engedélyezve!".format(mot+1))
     else:
-        print("Hibás engedélyezés: Motor{0} - Enable:{1}".format(mot, enable))
+        print("Hibás engedélyezés: Motor{0} - ENABLE:{1}!".format((mot+1), enable))
 
 
-def move(mot, level): #Mot számú motor lépésjel kiadás (fel vagy le)
+def move(mot, level):
+    """ Adott motor lépésjelének kiadása (fel vagy le) """
     if level == 1:
-        gpio.output(motor_gpio[mot-1], gpio.HIGH)
+        gpio.output(motor_gpio[mot], gpio.HIGH)
+        #print("Motor{0} kimenet magas!".format(mot+1))
     elif level == 0:
-        gpio.output(motor_gpio[mot-1], gpio.LOW)
+        gpio.output(motor_gpio[mot], gpio.LOW)
+        #print("Motor{0} kimenet alacsony!".format(mot+1))
+    else:
+        print("Hibás lépésjel: Motor{0} - LEVEL:{1}!".format((mot+1), level))
 
-
-def cleanup():
-    gpio.cleanup()
+def cleanup(): #GPIO pinout tisztítása
+     gpio.cleanup()
