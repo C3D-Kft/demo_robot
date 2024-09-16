0764569c17c3ebb5793b3aeaa66b97bfb6328658
NWiktor <wetzlvi@gmail.com>
2024-09-16 14:23:23 +0200

    update module functions

--- data_collector.py
+++ data_collector.py
@@ -11,27 +11,44 @@ www.C3D.hu
 
 # First import should be the logging module if any!
 import logging
+import threading
 import serial
-import queue
+from queue import Queue
 
 log = logging.getLogger("Main")
+que = Queue()
+FLAG = False
 
-# robot_control_gui - gomb hozzáadása - kapcsolat létrehozása / rögzítés elkezdése
-# gomb hozzáadása - rögzítése megállítása, fájl létrehozása
 
+def main():
+    ser = serial.Serial('COM11', 9600, timeout=5)
 
-def initialize():
-    ser = serial.Serial('/dev/ttyACM0', 9600)
-    s = [0]
     while True:
         read_serial = ser.readline()
-        s[0] = str(int(ser.readline(), 16))
-        print(s[0])
-        print(read_serial)
+        que.put(read_serial)
+        log.info(read_serial)
+        if not FLAG:
+            break
+
 
 def start_collect():
-    pass
+    global FLAG
+    FLAG = True
+    secondary_thread = threading.Thread(target=main)
+    secondary_thread.start()
+
 
 def stop_collect():
-    pass
+    global FLAG
+    FLAG = False
 
+
+def save_data(filename):
+
+    with open(filename, "w") as file:
+
+        while not que.empty():  # Writing data to a file
+            string = que.get().decode('utf-8').rstrip()
+            file.write(f"{string}\n")  # Empties the queue
+
+        log.info("File saved!")
