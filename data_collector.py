c028997ed41ba1cd3a27b88ebb0149aa5484e104
NWiktor <wetzlvi@gmail.com>
2024-09-17 10:55:48 +0200

    implement method to drop first few lines of data

--- data_collector.py
+++ data_collector.py
@@ -53,11 +53,28 @@ def stop_collect():
 
 
 def save_data(filename):
+    """ Write the contents of the queue to a specified file. """
 
     with open(filename, "w") as file:
-
+        found = False
+        log.info("Start processing queue...")
         while not que.empty():  # Writing data to a file
-            string = que.get().decode('utf-8').rstrip()
-            file.write(f"{string}\n")  # Empties the queue
+            # NOTE: Arduino serial uses 'raw' encoding, it is defined by the
+            #  code itself
+            # NOTE: Decoding errors are ignored, otherwise the code breaks
+            string = que.get().decode("ascii", "ignore")
+            log.info("Processing: {0}".format(string.rstrip()))
+            log.debug("Found: {0}".format(found))
+
+            # Search for header, and ignore all lines before that
+            if not found:
+                idx = string.find("Set")
+                log.debug("Found 'Set': {0}".format(idx))
+                if idx != -1:
+                    found = True
+
+            if found:
+                file.write(f"{string}")  # Empties the queue
 
         log.info("File saved!")
+
